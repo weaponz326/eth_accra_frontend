af2a86e046b93e79df7f9df229dfa5a0fec75fca
Weps <weaponz326@gmail.com>
2025-09-04 13:11:52 +0000

    shared nav components

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from '../../../core/services/auth/auth';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+  userAddress: string | null = null;
+  isPatient: boolean = false; // Placeholder: Determine role via contract
+  isProvider: boolean = false;
 
+  constructor(private authService: Auth, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authService.getUserAddress().subscribe(address => {
+      this.userAddress = address;
+      // TODO: Check PatientRegistry/ProviderRegistry to set isPatient/isProvider
+    });
+  }
+
+  connectWallet(): void {
+    this.authService.connectWallet();
+  }
+
+  disconnectWallet(): void {
+    this.authService.disconnect();
+    this.router.navigate(['/']);
+  }
 }
--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../../core/services/auth/auth';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.scss'
 })
 export class Sidebar {
+  isPatient: boolean = false;
+  isProvider: boolean = false;
+  isCollapsed: boolean = true;
 
+  constructor(private authService: Auth) {}
+
+  ngOnInit(): void {
+    this.authService.getUserAddress().subscribe(address => {
+      if (address) {
+        // TODO: Query contracts to set isPatient/isProvider
+      }
+    });
+  }
+
+  toggleSidebar(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
