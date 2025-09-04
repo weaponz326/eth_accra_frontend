9c9129e8c493c4c939d8e4790427e4b54b11d602
Weps <weaponz326@gmail.com>
2025-09-04 12:00:53 +0000

    environment vars

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  blockchainRpc: 'https://sepolia.infura.io/v3/YOUR_INFURA_KEY',
+  ipfsGateway: 'https://ipfs.infura.io:5001',
+  contractAddresses: {
+    patientRegistry: '0x...',
+    providerRegistry: '0x...',
+    records: '0x...',
+    auditTrail: '0x...',
+    billing: '0x...',
+    referralSystem: '0x...'
+  }
+};
